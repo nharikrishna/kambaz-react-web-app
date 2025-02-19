b2f072ef8cffdd8592d4c716bd5ecb8c2c1a05a8
nharikrishna <nharikrishna275@gmail.com>
2025-02-19 14:37:48 -0500

    minor changes

--- src/Kambaz/Account/Navigation.tsx
+++ src/Kambaz/Account/Navigation.tsx
@@ -5,21 +5,21 @@ export default function AccountNavigation() {
         <div id="wd-account-navigation" className="wd list-group fs-5 rounded-0">
             <NavLink
                 to={`/Kambaz/Account/Signin`}
-                className={({isActive}) => `list-group-item border border-0 ${isActive ? 'active' : 'inactive'}`}
+                className={({isActive}) => `list-group-item border border-0 ${isActive ? 'active text-danger' : 'inactive'}`}
             >
                 Signin
             </NavLink>
             <br/>
             <NavLink
                 to={`/Kambaz/Account/Signup`}
-                className={({isActive}) => `list-group-item border border-0 ${isActive ? 'active' : 'inactive'}`}
+                className={({isActive}) => `list-group-item border border-0 ${isActive ? 'active text-danger' : 'inactive'}`}
             >
                 Signup
             </NavLink>
             <br/>
             <NavLink
                 to={`/Kambaz/Account/Profile`}
-                className={({isActive}) => `list-group-item border border-0 ${isActive ? 'active' : 'inactive'}`}
+                className={({isActive}) => `list-group-item border border-0 ${isActive ? 'active text-danger' : 'inactive'}`}
             >
                 Profile
             </NavLink>
