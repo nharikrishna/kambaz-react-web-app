9919784776229fe3a7058ed8b9a93ae7e29e0d13
nharikrishna <nharikrishna275@gmail.com>
2025-04-02 01:18:59 -0400

    changes till 5.3

---
+++ src/Labs/Lab5/QueryParameters.tsx
@@ -0,0 +1,40 @@
+import {FormControl} from "react-bootstrap";
+import {useState} from "react";
+
+export default function QueryParameters() {
+    const REMOTE_SERVER = import.meta.env.VITE_REMOTE_SERVER;
+    const [a, setA] = useState("34");
+    const [b, setB] = useState("23");
+    return (
+        <div id="wd-query-parameters">
+            <h3>Query Parameters</h3>
+            <FormControl id="wd-query-parameter-a"
+                         className="mb-2"
+                         defaultValue={a} type="number"
+                         onChange={(e) => setA(e.target.value)}/>
+            <FormControl id="wd-query-parameter-b"
+                         className="mb-2"
+                         defaultValue={b} type="number"
+                         onChange={(e) => setB(e.target.value)}/>
+            <a className="btn btn-danger me-2" id="wd-query-parameter-add"
+               href={`${REMOTE_SERVER}/lab5/calculator?operation=add&a=${a}&b=${b}`}>
+                Add {a} + {b}
+            </a>
+            <a className="btn btn-primary me-2" id="wd-query-parameter-subtract"
+               href={`${REMOTE_SERVER}/lab5/calculator?operation=subtract&a=${a}&b=${b}`}>
+                Subtract {a} - {b}
+            </a>
+            <a className="btn btn-danger me-2" id="wd-query-parameter-subtract"
+               href={`${REMOTE_SERVER}/lab5/calculator?operation=multiply&a=${a}&b=${b}`}>
+                Multiply {a} * {b}
+            </a>
+            <a className="btn btn-primary" id="wd-query-parameter-subtract"
+               href={`${REMOTE_SERVER}/lab5/calculator?operation=divide&a=${a}&b=${b}`}>
+                Divide {a} / {b}
+            </a>
+            <hr/>
+        </div>
+
+
+    )
+}
