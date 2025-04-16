7772fcb6fdb737b18cb4ce57e9d432f9c3b532f9
nharikrishna <nharikrishna275@gmail.com>
2025-04-16 19:17:35 -0400

    minor change

--- src/Kambaz/Courses/People/Table.tsx
+++ src/Kambaz/Courses/People/Table.tsx
@@ -1,35 +1,52 @@
 import { Table } from "react-bootstrap";
 import { FaUserCircle } from "react-icons/fa";
 import PeopleDetails from "./Details.tsx";
-import {Link} from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
-export default function PeopleTable({users = []}: {users?: any}) {
+export default function PeopleTable({ users = [] }: { users?: any }) {
+    const location = useLocation();
+    const isAccountUsersPage = location.pathname.startsWith("/Kambaz/Account/Users");
+
     return (
         <div id="wd-people-table">
-            <PeopleDetails/>
+            <PeopleDetails />
             <Table striped>
                 <thead>
-                <tr><th>Name</th><th>Login ID</th><th>Section</th><th>Role</th><th>Last Activity</th><th>Total Activity</th></tr>
+                <tr>
+                    <th>Name</th><th>Login ID</th><th>Section</th><th>Role</th><th>Last Activity</th><th>Total Activity</th>
+                </tr>
                 </thead>
                 <tbody>
-                    {users.map((user: any) => (
-                            <tr key={user._id}>
-                                <td className="wd-full-name text-nowrap">
+                {users.map((user: any) => {
+                    const nameContent = (
+                        <>
+                            <FaUserCircle className="me-2 fs-1 text-secondary" />
+                            <span className="wd-first-name">{user.firstName}</span>
+                            <span className="wd-last-name">{user.lastName}</span>
+                        </>
+                    );
+
+                    return (
+                        <tr key={user._id}>
+                            <td className="wd-full-name text-nowrap">
+                                {isAccountUsersPage ? (
                                     <Link to={`/Kambaz/Account/Users/${user._id}`} className="text-decoration-none">
-                                        <FaUserCircle className="me-2 fs-1 text-secondary" />
-                                        <span className="wd-first-name">{user.firstName}</span>
-                                        <span className="wd-last-name">{user.lastName}</span>
+                                        {nameContent}
                                     </Link>
-                                </td>
-                                <td className="wd-login-id">{user.loginId}</td>
-                                <td className="wd-section">{user.section}</td>
-                                <td className="wd-role">{user.role}</td>
-                                <td className="wd-last-activity">{user.lastActivity}</td>
-                                <td className="wd-total-activity">{user.totalActivity}</td>
-                            </tr>
-                        ))}
+                                ) : (
+                                    nameContent
+                                )}
+                            </td>
+                            <td className="wd-login-id">{user.loginId}</td>
+                            <td className="wd-section">{user.section}</td>
+                            <td className="wd-role">{user.role}</td>
+                            <td className="wd-last-activity">{user.lastActivity}</td>
+                            <td className="wd-total-activity">{user.totalActivity}</td>
+                        </tr>
+                    );
+                })}
                 </tbody>
             </Table>
-        </div>);
+        </div>
+    );
 }
-
